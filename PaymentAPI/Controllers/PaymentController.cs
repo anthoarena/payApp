1c7d782fa9241434f0285a2d77f176289b2b22be
anthony <arena.antho@hotmail.com>
2020-10-13 18:30:37 +0200

    Create Api Controller

---
+++ PaymentAPI/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase {
+
+        public PaymentController() {
+
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Payment(int id) {
+            return Ok(id);
+        }
+
+    }
+}
