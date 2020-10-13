870ad0acead669d7aa82dc2bf1a1151319bf1988
anthony <arena.antho@hotmail.com>
2020-10-13 19:44:49 +0200

    Initialization test Api project - Test Controller

---
+++ PaymentAPITestProject/TestPaymentAPI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PaymentAPITestProject {
+    public class TestPaymentAPI : IClassFixture<AppTestFixture> {
+
+        private readonly AppTestFixture _fixture;
+        private readonly HttpClient _client;
+
+
+        public TestPaymentAPI(AppTestFixture fixture) {
+            _fixture = fixture;
+            _client = fixture.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_Payment_ID_ValidInputSuccess() {
+            //Arrange
+            int paymentId = 1;
+
+            //Act
+            var response = await _client.GetAsync($"api/payment/{paymentId}");
+
+            //Assert1
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
